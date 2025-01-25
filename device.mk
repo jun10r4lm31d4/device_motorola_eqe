a22eb8d638c4bcfd959558741e0f5fa5b6c5a175
Michael Bestas <mkbestas@lineageos.org>
2025-01-25 20:33:26 +0200

    eqe: Set speaker protection calibration path
    Change-Id: I60f93feeeff74e30d896be50aa7e799ac045f7d5

--- device.mk
+++ device.mk
@@ -83,6 +83,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
 $(call soong_config_set_bool,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
+$(call soong_config_set,qtiaudio,pal_spkr_protection_path,/mnt/vendor/persist/factory/audio/audio.cal)
 
 # Bluetooth
 PRODUCT_PACKAGES += \

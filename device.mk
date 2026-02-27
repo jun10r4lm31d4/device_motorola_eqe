f95716812b878c29d2eaff2ba934ea6910242a99
NikitaSavc <NikitaSavc@users.noreply.github.com>
2026-02-26 21:51:06 -0300

    eqe: Use sensors HAL from hardware/motorola
    Change-Id: I90a3dd06c3f949ee14e434baf859bd6d1a874729

--- device.mk
+++ device.mk
@@ -311,7 +311,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.multihal \
     sensors.dynamic_sensor_hal \
-    sensors.eqe
+    sensors.moto_ext
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/sku_crow/android.hardware.sensor.accelerometer.xml \

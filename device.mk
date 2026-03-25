00cb9e2623ac9af63f7c71fdb127e02932d7bd87
Júnior Almeida <jun10r4lm31d4@outlook.com>
2026-03-24 21:37:06 -0300

    eqe: drop Google eUICC and add OpenEUICC package

--- device.mk
+++ device.mk
@@ -250,6 +250,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
+# OpenEUICC
+PRODUCT_PACKAGES += \
+    OpenEUICC
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay-lineage

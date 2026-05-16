bd0f35f1c74d9b3df66c2d521c457dff210c3e9c
Júnior Almeida <jun10r4lm31d4@outlook.com>
2026-05-16 10:58:01 -0300

    eqe: Switch to NCM USB tethering

--- device.mk
+++ device.mk
@@ -284,6 +284,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
+    NcmTetheringOverlay
+
+PRODUCT_PACKAGES += \
     CarrierConfigResCommon_Sys \
     FrameworksResCommon_Sys \
     NfcResCommon_Sys \
